e3007f1d15eef879968a10d4b486151d2240bbe0
qmohona <46950150+Tasnim-Mohona@users.noreply.github.com>
2023-08-09 12:37:11 +0600

    Add files via upload

---
+++ student_record_management.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#include<windows.h>
+
+#define Student struct Stud
+
+void add(FILE*fp);
+void modify(FILE*fp);
+void display(FILE*fp);
+void Indivisual(FILE*fp);
+void password();
+FILE*delo(FILE*fp);
+void printchar(char ch, int n);
+void title();
+FILE *tp;
+
